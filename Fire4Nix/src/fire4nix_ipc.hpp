52be511ee53de7e242c00c70e15bf4d43f21eca2
Baltdh <85966468+Baltdh@users.noreply.github.com>
2026-09-21 17:56:55 -0300

    Store command IPC endpoint in CommandPipe

--- Fire4Nix/src/fire4nix_ipc.hpp
+++ Fire4Nix/src/fire4nix_ipc.hpp
@@ -1,8 +1,12 @@
 #pragma once
+#include <filesystem>
 #include <string>
 
 class CommandPipe {
 public:
     bool create(const std::string& baseName);
     bool sendCommand(const std::string& cmd);
+
+private:
+    std::filesystem::path path_;
 };
